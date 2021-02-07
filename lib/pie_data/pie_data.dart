ff06bc40b52c58961fad1a26c6baa618bafda446
Emir Kalburcu <ekalburcu@gmail.com>
2021-02-07 20:27:03 +0300

    Update pie_data.dart

--- lib/pie_data/pie_data.dart
+++ lib/pie_data/pie_data.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
 
+
+//Veli Hocam, Pie Chart kullanmak istemistimm projede, "fl_chart" packageini kullandim bunun icin, 
+//ama layoutlardan dolayi istedigim performansi vermedi, o nedenle bu kodlar pafis haldedir ama Pie Chart calisiyor.
+
+
+
 class PieData {
   static List<Data> data = [
     Data(name: 'Blue', percent: 40, color: const Color(0xff0293ee)),
